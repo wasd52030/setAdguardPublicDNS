e7063662b01cbe8389f7c0a4718e8425aaece682
sobol <nppstcc1001050311@gmail.com>
2024-05-08 10:41:13 +0800

    no message

--- setDNS.ps1
+++ setDNS.ps1
@@ -1,26 +1,50 @@
-$context = New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
+param (
+    $type
+)
 
 # 檢查是否已經以 Administrator 身份執行
-if (-not $context.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    # 向使用者顯示 UAC 提示，請求 Administrator 權限
-    $process = New-Object System.Diagnostics.ProcessStartInfo
-    $process.FileName = "powershell.exe" # 使用 PowerShell 可执行文件
-    $process.Arguments = "-File `"$($MyInvocation.MyCommand.Path)`"" # 将当前脚本路径作为参数传递给 PowerShell
-    $process.Verb = "runas" 
-    [System.Diagnostics.Process]::Start($process) | Out-Null
-    Exit
+function CheckAdministrator {
+    $context = New-Object System.Security.Principal.WindowsPrincipal([System.Security.Principal.WindowsIdentity]::GetCurrent())
+    if (-not $context.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        $scriptPath = $MyInvocation.ScriptName
+        $process = New-Object System.Diagnostics.ProcessStartInfo
+        $process.FileName = "powershell.exe"
+        $process.Arguments = "-File `"$scriptPath`" -type $type"
+        $process.Verb = "runas"
+        [System.Diagnostics.Process]::Start($process) | Out-Null
+        Exit
+    }
 }
 
-$q=Get-NetIPInterface | Where-Object {
-    $IPInterfaceId=$_.InterfaceIndex
-    
-    $ip4Preferred="94.140.14.14"
-    $ip4Alternate="94.140.15.15"
-    Set-DnsClientServerAddress -InterfaceIndex $IPInterfaceId -ServerAddresses $ip4Preferred,$ip4Alternate
+function SetDNSAddresses {
+    Get-NetIPInterface | Where-Object {
+        $IPInterfaceId = $_.InterfaceIndex
 
-    $ip6Preferred="2a10:50c0::ad1:ff"
-    $ip6Alternate="2a10:50c0::ad2:ff"
-    Set-DnsClientServerAddress -InterfaceIndex $IPInterfaceId -ServerAddresses $ip6Preferred,$ip6Alternate
+        $ip4Preferred = "94.140.14.14"
+        $ip4Alternate = "94.140.15.15"
+        Set-DnsClientServerAddress -InterfaceIndex $IPInterfaceId -ServerAddresses $ip4Preferred, $ip4Alternate
+
+        $ip6Preferred = "2a10:50c0::ad1:ff"
+        $ip6Alternate = "2a10:50c0::ad2:ff"
+        Set-DnsClientServerAddress -InterfaceIndex $IPInterfaceId -ServerAddresses $ip6Preferred, $ip6Alternate
+    }
+}
+
+function ResetDNSAddresses {
+    Get-NetIPInterface | Where-Object {
+        $IPInterfaceId = $_.InterfaceIndex
+
+        Set-DnsClientServerAddress -InterfaceIndex $IPInterfaceId -ResetServerAddresses
+        Set-DnsClientServerAddress -InterfaceIndex $IPInterfaceId -ResetServerAddresses
+    }
+}
+
+CheckAdministrator
+
+if ($type -eq "set") {
+    SetDNSAddresses
+} elseif ($type -eq "reset") {
+    ResetDNSAddresses
 }
 
 ipconfig /release
